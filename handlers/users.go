d3c3971b446e5f261a1bde3f13a93b295150b034
Dev-Tams <tammyalemu@gmail.com>
2025-05-01 19:16:26 +0100

    added a put method to handle users

--- handlers/users.go
+++ handlers/users.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"net/http"
 	"strconv"
-
 	"github.com/dev-tams/note-go/db"
 	"github.com/dev-tams/note-go/models"
 	"github.com/gin-gonic/gin"
@@ -57,6 +56,51 @@ func CreateUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, user)
+}
+
+func UpdateUser(c *gin.Context ){
+	
+	var user models.User
+
+	//find user by id 
+
+	idParam :=c.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+
+	//check input
+	if err != nil{
+		c.JSON(http.StatusNotAcceptable, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	//find user
+	if err := db.DB.Find(&user, id).Error; 
+	err != nil{
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found!"})
+		return
+	}
+
+	var updatedData models.User
+	if err := c.ShouldBindJSON(&updatedData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	user.Name = updatedData.Name
+	user.Email = updatedData.Email
+
+	
+	//save and return
+	if err := db.DB.Save(&user).Error;
+	err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+	}
+
+
+	c.JSON(http.StatusOK, gin.H{"message": "User updated", "data": user})
+
+
 }
 
 func DeleteUser(c *gin.Context) {
--- main.go
+++ main.go
@@ -33,5 +33,6 @@ func RegisterRoutes(r *gin.Engine) {
 	r.GET("/users", handlers.GetUsers)
 	r.GET("/users/:id", handlers.GetUserByID)
 	r.POST("/users", handlers.CreateUser)
+	r.PUT("/users/:id", handlers.UpdateUser)
 	r.DELETE("/users/:id", handlers.DeleteUser)
 }
