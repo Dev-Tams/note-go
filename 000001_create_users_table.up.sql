20247137604992cd3a9587a7acdfa6c3371205d6
Dev-Tams <tammyalemu@gmail.com>
2025-05-10 22:57:54 +0100

    minor app fixez

--- 000001_create_users_table.up.sql
+++
@@ -1,6 +0,0 @@
--- CREATE TABLE users (
---     id INTEGER PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     email VARCHAR(100) UNIQUE NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
