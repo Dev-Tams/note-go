5946d3786ac1f3016d9e1d84e8fa99936fb65632
Dev-Tams <tammyalemu@gmail.com>
2025-05-10 22:55:45 +0100

    added note migration

---
+++ db/migrations/000002_create_notes_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE notes (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    -- FOREIGN KEY (user_id) REFERENCES users(id)
+);
